969f231bc7dd9914820991ea12b568d684a05668
alerebal <alerebal@gmail.com>
2021-01-30 13:01:41 +0000

    after splide, before toggle-button

--- src/main.js
+++ src/main.js
@@ -2,3 +2,26 @@
 function toScroll(where) {
     document.getElementById(where).scrollIntoView({behavior: "smooth"})
 }
+
+function width() {
+    if(innerWidth < 700) {
+        document.addEventListener("DOMContentLoaded", function () {
+            new Splide(".splide", {
+                perPage: 1,
+                perMove: 1,
+                rewind: true,
+            }).mount();
+        });
+    } else {
+        document.addEventListener("DOMContentLoaded", function () {
+            new Splide(".splide", {
+                perPage: 3,
+                perMove: 1,
+                rewind: true,
+            }).mount();
+        });
+    }
+}
+
+width()
+
