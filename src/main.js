b99e940c1a925e3b12d5dcc936e9ddd759ee6bba
alerebal <ale.rebal@gmail.com>
2021-07-23 12:22:14 +0100

    video 1

--- src/main.js
+++ src/main.js
@@ -1,30 +1,41 @@
+// Video
+
+const side1 = document.getElementById('side1');
+const side2 = document.getElementById('side2');
+
 // Parallax
 
-const parallaxBg = document.querySelector(".parallax-bg");
+// const parallaxBg = document.querySelector(".parallax-bg");
 const parallaxBox = document.querySelector(".parallax-box")
+
+// Scroll effects
 window.addEventListener("scroll", function () { 
     let offset = window.pageYOffset;
-    parallaxBg.style.backgroundPositionY = offset * 0.7 + 'px'
-    parallaxBox.style.top =  offset * 0.15 + 'px'
+    // parallaxBg.style.backgroundPositionY = offset * 0.7 + 'px'
+    side1.style.left = -offset + 'px';
+    side2.style.left = offset + 'px';
+    parallaxBox.style.top =  offset * 0.075 + 'px'
  })
 
 // title change color
 const title = document.querySelector('.nameTitle');
-const sectionNav = document.querySelector('#nav-bar')
+const sectionNav = document.querySelector('.nameTitle')
 
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
 
 const sectionNavOptions = {
-    rootMargin: "-450px 0px 0px 0px"
+    rootMargin: "-400px 0px 0px 0px"
 };
 
 const sectionNavObserver = new IntersectionObserver(function (entries) {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
             title.classList.add("changeTitle")
+
         } else {
             title.classList.remove("changeTitle")
+
         }
     })
 }, sectionNavOptions)
